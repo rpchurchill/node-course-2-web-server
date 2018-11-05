e4d68e9359e417bd086ec7aa7d9101a02db8c3b2
Robert P. Churchill <products@rpchurchill.com>
2018-11-05 17:53:55 -0500

    correct spelling of PORT function

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.Port || 3000;  //for Heroku deployment
+const port = process.env.PORT || 3000;  //for Heroku deployment
 
 var app = express();
 
